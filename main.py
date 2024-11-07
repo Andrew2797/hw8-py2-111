31b82d551801e8ceec42c7ae883edb22e0bcf8df
Andrew2797 <andrijgolub27@gmail.com>
2024-11-07 18:02:51 +0200

    hw9

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 from routes.pizza import pizza_route
 from models.pizza import Pizza
@@ -7,9 +7,28 @@ from models.base import create_db
 
 
 app = Flask(__name__)
+
+
 app.register_blueprint(pizza_route)
+pizzas = [
+    {"id": 1, "name": "Маргарита", "price": 150, "votes": 0},
+    {"id": 2, "name": "Пепероні", "price": 180, "votes": 0}
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", pizzas=pizzas)
+
+
+@app.route("/vote", methods=["POST"])
+def vote():
+    pizza_name = request.form.get("pizza_choice")
+    for pizza in pizzas:
+        if pizza["name"] == pizza_name:
+            pizza["votes"] += 1
+            break
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
-    create_db()
     app.run(debug=True)
